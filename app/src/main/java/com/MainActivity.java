bb945d2b1d1132aaa35654840bee0027dee0fd18
Sergey Zakharov <zerxgm@yandex.ru>
2021-06-17 21:16:18 +0500

    first commit

---
+++ app/src/main/java/com/MainActivity.java
@@ -0,0 +1,16 @@
+package com;
+
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.takingpills.R;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
